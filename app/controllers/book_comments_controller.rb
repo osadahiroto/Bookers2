4d93ff81a7dc555648b53a753fb9bf47c6723bc8
osadahiroto <hiroumi879C@gmail.com>
2021-09-05 05:03:47 +0000

    [Finish] 2-3

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -11,12 +11,12 @@ class BookCommentsController < ApplicationController
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
     redirect_to book_path(params[:book_id])
   end
-  
+
   private
 
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-  
+
 end
 
